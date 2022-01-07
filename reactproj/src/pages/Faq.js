dba8f5a719ebf0e9029ec2901f195cca5df59c16
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2022-01-07 13:27:30 +0800

    added faqsand styles folder

--- reactproj/src/pages/Faq.js
+++ reactproj/src/pages/Faq.js
@@ -1,12 +1,58 @@
 import React from 'react';
+import FAQBG from '../components/images/faqs-bg.png';
+import "../components/styles/FAQs.css"
 
 function Faq() {
    return (
       <div>
-         <h1>Faq</h1>
-         <p>
-            why here?
-         </p>
+         <div className="hero">
+            <div className="hero-text">
+                  {/* <h1 className="ht">WE ARE <br/> HERE TO <br/> <span>HELP</span>.</h1> */}
+            </div>
+         </div>
+         <div id="FAQ">
+            <h3 className="faq-title">FREQUENTLY ASKED QUESTIONS</h3>
+            <p className="faq-text">Need some help with your order or got a burning question that you need answered pronto?
+               Just click on one of the sections below to find the answer.
+            </p>
+            <figure>
+               <img className="bg-img" src={FAQBG} alt="musical note">
+               </img>
+            </figure>
+
+            <div class="container-sm accordion accordion-flush" id="accordionFlushExample">
+               <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingOne">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Accordion Item #1
+                  </button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                     <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                  </div>
+               </div>
+               <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        Accordion Item #2
+                  </button>
+                  </h2>
+                  <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                     <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                  </div>
+               </div>
+               <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        Accordion Item #3
+                  </button>
+                  </h2>
+                  <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                     <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                  </div>
+               </div>
+            </div> 
+         </div>
       </div>
    );
 }

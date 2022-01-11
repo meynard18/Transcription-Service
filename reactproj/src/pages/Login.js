301cb59ecf40869ed134687207b9bd088b091f3e
Meynard Duran <pvt_mduran18@yahoo.com>
2022-01-11 15:34:17 +0800

    rearrange page format and added hash feature on password, login in progress
     Author:    Meynard Duran <pvt_mduran18@yahoo.com>
     interactive rebase in progress; onto 354845e
     Last command done (1 command done):
       pick 49b331a rearrange page format and added hash feature on password, login in progress
     Next command to do (1 remaining command):
       pick f41b8cf added json file
     You are currently rebasing branch 'lorie' on '354845e'.
    
    Changes to be committed:
    	modified:   reactproj/src/App.js
    	modified:   reactproj/src/components/Navbar.js
    	modified:   reactproj/src/pages/AboutUs.js
    	modified:   reactproj/src/pages/Home.js
    	modified:   reactproj/src/pages/Login.js
    	deleted:    reactproj/src/pages/Transcriptions.js
    	modified:   transcription/app/Http/Controllers/API/AccountController.php
    	modified:   transcription/routes/api.php

--- reactproj/src/pages/Login.js
+++ reactproj/src/pages/Login.js
@@ -1,64 +1,86 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import email from "../components/images/email.png";
-import lock from "../components/images/lock.png";
-import UseForm from "../components/UseForm";
-import styles from "../components/styles/Login.module.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import email from '../components/images/email.png';
+import lock from '../components/images/lock.png';
+import UseForm from '../components/UseForm';
+import styles from '../components/styles/Login.module.css';
+import axios from 'axios';
 
-function Login({ submitForm }) {
+function Login() {
+   const [logInInput, setLogIn] = useState({
+      email: '',
+      password: '',
+   });
 
-   const { handleChange, handleFormSubmit, values, errors } = UseForm(
-      submitForm
-   );
+   const handleInput = (e) => {
+      e.persist();
+      setLogIn({ ...logInInput, [e.target.name]: e.target.value });
+   };
+
+   const logInSubmit = (e) => {
+      e.preventDefault();
+      const data = {
+          email: logInInput.email,
+          password: logInInput.password, 
+      }
+      axios.post(`api/login`, data).then(res => {
+
+      })
+   }
 
    return (
       <div className={styles.content}>
-         {/* <div }> */}
-         <form className={styles.main2}>
+         <form onSubmit={logInSubmit} className={styles.main2}>
             <div className={styles.main}>
                <div className={styles.subMain}>
                   <div className={styles.center}>
-                     {/* <div style={imgs}>
-                        <div >
-                           <img src={profile} alt="profile" style={profileIcon} />
-                        </div>
-                     </div> */}
                      <div>
                         <h1 className={styles.lHeader}>Login</h1>
                         <div>
-                           <img src={email} alt="email" className={styles.emailIcon} />
+                           <img
+                              src={email}
+                              alt="email"
+                              className={styles.emailIcon}
+                           />
                            <input
                               type="email"
-                              placeholder="Enter Email-id"
-                              // className="fill"
+                              placeholder="Enter Email Address"
                               className={styles.fill}
                               name="email"
                               required
-                              value={values.email}
-                              onChange={handleChange}
-                           // onClick={handleFormSubmit}
+                              onChange={handleInput}
                            />
-                           {errors.email && <p className={styles.error}>{errors.email}</p>}
+                
                         </div>
                         <div className={styles.secondInput}>
-                           <img src={lock} alt="password" className={styles.emailIcon} />
+                           <img
+                              src={lock}
+                              alt="password"
+                              className={styles.emailIcon}
+                           />
                            <input
                               type="password"
                               placeholder="Enter Password"
                               className={styles.fill}
                               name="password"
                               required
-                              value={values.password}
-                              onChange={handleChange}
-
+                              // value={values.password}
+                              onChange={handleInput}
                            />
-                           {errors.password && <p className={styles.error}>{errors.password}</p>}
+                           {/* {errors.password && (
+                              <p className={styles.error}>{errors.password}</p>
+                           )} */}
                         </div>
 
                         <div className={styles.btn}>
                            <Link to="/transcription">
-                              <button className={styles.loginBtn} type="button" onClick={handleFormSubmit}>Login</button>
-
+                              <button
+                                 className={styles.loginBtn}
+                                 type="button"
+                                 // onClick={handleFormSubmit}
+                              >
+                                 Login
+                              </button>
                            </Link>
                         </div>
 
@@ -67,7 +89,6 @@ function Login({ submitForm }) {
                               <li>Register Now</li>
                            </Link>
                         </div>
-
                      </div>
                   </div>
                </div>

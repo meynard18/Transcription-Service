9a19efc2009236bfff49827fc07fb553051698c5
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2022-01-14 12:05:04 +0800

    added styles for transcription page

--- reactproj/src/components/AddCart.js
+++ reactproj/src/components/AddCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCart } from 'react-use-cart';
+import '../components/styles/AddCart.css';
 
 const AddCart = () => {
     const {
@@ -14,20 +15,18 @@ const AddCart = () => {
       } = useCart();
     
       if (isEmpty) {
-          return <p>Your cart is empty</p>;
+          return <p className="toCart">Your cart is empty</p>;
       }
 
 
     return (
         <div>
-            <div>
+            <div className="toCart">
                 <h5>Cart: ({totalUniqueItems}) Total Items: ({totalItems})</h5> 
                 <div>
-                    <table className="table table-striped container-sm">
+                    <table className="table table-striped container-sm toCart">
                         <thead className="text-center">
                             <th>Ordered Item</th>
-                            <th>Quantity</th>
-                            <th>Action</th>
                         </thead>
                         {items.map(item => 
                             (
@@ -35,15 +34,15 @@ const AddCart = () => {
                                     <td>
                                         <ul>
                                             <li>
-                                                <p>Title: {item.song_title}</p>
-                                                <p>Artist: {item.artist_name}</p>
+                                                <p>Title: {item.title}</p>
+                                                <p>Artist: {item.artist}</p>
                                                 <p>Category: {item.category}</p>
                                                 <p>Price: P{item.price}</p>
                                             </li>
                                         </ul>    
                                     </td>
                                     <td>
-                                        <button 
+                                        {/* <button 
                                             className="btn btn-default"
                                             onClick = {() => updateItemQuantity(item.id, item.quantity + 1)}
                                         >
@@ -55,24 +54,24 @@ const AddCart = () => {
                                             onClick = {() => updateItemQuantity(item.id, item.quantity - 1)}
                                         >
                                             <i class="bi bi-dash-circle"></i>
-                                        </button>   
+                                        </button>    */}
                                     </td>
                                     <td className="actions">
                                         <button 
                                         className="btn btn-default"
                                         onClick = {() => removeItem(item.id)}
                                         >
-                                        <i class="bi bi-trash"></i>
+                                        <i class="bi bi-trash toCart"></i>
                                         </button>
                                     </td>
                                 </tr>         
                             ))}
                     </table>
                 </div>
-                <div className="container-sm d-flex justify-content-end">
+                <div className="container-sm d-flex justify-content-end toCart">
                     <h2>Total Price: P{cartTotal}</h2>
                 </div>
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <button
                     className="btn btn-danger"
                     onClick = {() => emptyCart()}
@@ -82,7 +81,7 @@ const AddCart = () => {
                     <button
                     className="btn btn-warning"
                     >
-                        Proceed to Checkout
+                        Checkout
                     </button>
                 </div>
                 
